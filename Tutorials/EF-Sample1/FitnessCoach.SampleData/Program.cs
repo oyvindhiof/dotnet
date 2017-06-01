b008a0c053066b0ce5d20789e8e5c18fe772b636
oyvindhiof <oyvind.ohra@communicate.no>
2017-06-01 22:48:12 +0200

    Added JSON

--- Tutorials/EF-Sample1/FitnessCoach.SampleData/Program.cs
+++ Tutorials/EF-Sample1/FitnessCoach.SampleData/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace FitnessCoach.SampleData
+﻿//using FitnessCoach.Model;
+//using Newtonsoft.Json;
+using System.IO;
+
+namespace FitnessCoach.SampleData
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var directory = Path.GetDirectoryName(path);
+            var workoutFilePath = Path.Combine(directory, @"Data\workouts.json");
+
+            //var workouts = JsonConvert.DeserializeObject<Workout[]>(File.ReadAllText(workoutFilePath));
+            //// sculi buli
         }
     }
 }
